1f1aaac3c5b2f35a87bc9e13e4ec9d455275033c
Raghunath <raghunathec@gmail.com>
2024-11-24 20:12:19 +0530

    updated flutter screens

--- quick_task_app/lib/main.dart
+++ quick_task_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-//import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
 import 'screens/signin_screen.dart';
 
 void main() async {
